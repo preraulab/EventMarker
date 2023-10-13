d7e7247337fb57efce5744b7a38d6a009d31aa1b
Michael Prerau, Ph.D <78376124+prerau@users.noreply.github.com>
2023-10-13 11:31:54 -0400

    Update AnnotationObject.m

--- AnnotationObject.m
+++ AnnotationObject.m
@@ -1,5 +1,29 @@
 classdef AnnotationObject < EventObject
-    
+% AnnotationObject - A class representing an annotation object for use with
+% EventMarker. Extends EvenObject
+%
+%   Usage:
+%       obj = AnnotationObject(name, type_ID, region, constrain)
+%
+%   Properties (Access = public):
+%       name: Text name of the event type
+%       type_ID: Event type ID
+%       region: Boolean indicating if it's a region (default: false)
+%       event_ID: Unique event ID
+%       constrain: If a region, allows for vertical constraint (default: true)
+%       obj_handle: Handle to the graphical object representing the event
+%       label_handle: Handle to the text label associated with the event
+%
+%   Methods (Access = public):
+%       AnnotationObject: Constructor for creating an EventObject
+%       time_bounds: Get the time bounds of the event object
+%
+% Example:
+%   see basic_viewer.m
+%
+% Copyright 2023 Michael J. Prerau Laboratory. - http://www.sleepEEG.org
+% ********************************************************************    
+
     %%%%%%%%%%%%%%%% public properties %%%%%%%%%%%%%%%%%%
     properties (Access = public)
         
